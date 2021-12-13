26a722d973eceb9ca01fcdd51e268d216dafd68c
Roan Hofland <roan@roanh.dev>
2021-12-13 16:41:51 +0100

    Refactor general program structure

---
+++ ConvexMerger/src/dev/roanh/convexmerger/game/ConvexMerger.java
@@ -0,0 +1,18 @@
+package dev.roanh.convexmerger.game;
+
+import javax.swing.JFrame;
+
+public class ConvexMerger{
+	private JFrame frame = new JFrame("Convex Merger");
+
+	
+	
+	
+	
+	
+	public static void showGame(){
+		
+		
+		
+	}
+}
--- ConvexMerger/src/dev/roanh/convexmerger/gui/GameRenderer.java
+++ ConvexMerger/src/dev/roanh/convexmerger/gui/GameRenderer.java
@@ -2,24 +2,22 @@ package dev.roanh.convexmerger.gui;
 
 import java.awt.Graphics;
 
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import dev.roanh.convexmerger.game.GameState;
 
-public class GameRenderer{
-	private JFrame frame = new JFrame("Convex Merger");
+public class GameRenderer extends JPanel{
+	/**
+	 * Serial ID.
+	 */
+	private static final long serialVersionUID = -3795126566870826056L;
 	private GameState game;
 	
 	
 	
 	
-	private static class GamePanel extends JPanel{
+	@Override
+	public void paintComponent(Graphics g){
 		
-		
-		@Override
-		public void paintComponent(Graphics g){
-			
-		}
 	}
 }
