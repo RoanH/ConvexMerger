285337351803431c64b2d29342aa0a1da92deb0e
Roan Hofland <roan@roanh.dev>
2021-12-13 16:12:17 +0100

    Write a template with contracts for the VerticalDecomposition class

--- ConvexMerger/src/dev/roanh/convexmerger/game/VerticalDecomposition.java
+++ ConvexMerger/src/dev/roanh/convexmerger/game/VerticalDecomposition.java
@@ -1,5 +1,75 @@
 package dev.roanh.convexmerger.game;
 
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+import java.util.List;
+
+/**
+ * Vertical decomposition of a plane containing non-overlapping
+ * convex objects. The decomposition supports efficient modifications
+ * and querying of the convex objects.
+ * @author Emu
+ */
 public class VerticalDecomposition{
 
+	/**
+	 * Constructs a new vertical decomposition with
+	 * the given bounding box.
+	 * @param bounds A bounding box that all objects that will
+	 *        ever be added will be contained in (strictly inside,
+	 *        there will be no overlap with the edges).
+	 */
+	public VerticalDecomposition(Rectangle2D.Double bounds){
+		//TODO
+	}
+	
+	/**
+	 * Adds a new convex object to this vertical decomposition.
+	 * @param obj The convex object to add.
+	 */
+	public void addObject(ConvexObject obj){
+		//TODO
+	}
+	
+	/**
+	 * Removes a convex object from this vertical decomposition.
+	 * @param obj The convex object to remove.
+	 */
+	public void removeObject(ConvexObject obj){
+		//TODO
+	}
+	
+	/**
+	 * Rebuilds the vertical composition to match the
+	 * current set of stored convex objects.
+	 */
+	public void rebuild(){
+		//TODO optional, you can assume that multiple object additions/removals happen at the same time,
+		//this method will be called when there will be no more changes meaning you
+		//could rebuild once instead of on each object change. You can delete this method if you decide
+		//its more efficient to just do everything in add/remove
+	}
+	
+	/**
+	 * Queries the convex object that is at the given position.
+	 * If no such object exists <code>null</code> is returned.
+	 * @param x The x-coordinate of the query point.
+	 * @param y The y-coordinate of the query point.
+	 * @return The convex object at the given position or
+	 *         <code>null</code> is there is no point at
+	 *         the given position.
+	 */
+	public ConvexObject queryObject(double x, double y){
+		return null;//TODO
+	}
+	
+	/**
+	 * Gets a list of lines that represent the vertical
+	 * decomposition lines. Lines to represent the bounding
+	 * box will be included as well.
+	 * @return The vertical decomposition lines.
+	 */
+	public List<Line2D.Double> getDecompLines(){
+		return null;//TODO
+	}
 }
