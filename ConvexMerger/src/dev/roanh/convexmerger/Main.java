641d95e69a9b5993b2256c1d8539af29ebc69ad6
Roan Hofland <roan@roanh.dev>
2021-12-13 16:45:01 +0100

    Empty game window

--- ConvexMerger/src/dev/roanh/convexmerger/Main.java
+++ ConvexMerger/src/dev/roanh/convexmerger/Main.java
@@ -1,8 +1,12 @@
 package dev.roanh.convexmerger;
 
+import dev.roanh.convexmerger.gui.ConvexMerger;
+import dev.roanh.util.Util;
+
 public class Main{
 
 	public static void main(String[] args){
-		
+		Util.installUI();
+		ConvexMerger.showGame();
 	}
 }
---
+++ ConvexMerger/src/dev/roanh/convexmerger/gui/ConvexMerger.java
@@ -0,0 +1,30 @@
+package dev.roanh.convexmerger.gui;
+
+import javax.swing.JFrame;
+
+public class ConvexMerger{
+	private static JFrame frame = new JFrame("Convex Merger");
+
+	
+	
+	
+	
+	
+	public static void showGame(){
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 600);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+}
