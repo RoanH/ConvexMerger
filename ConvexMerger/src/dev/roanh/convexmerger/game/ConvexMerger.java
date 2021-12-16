b378dd451146f9c3260cbf2bc4244eb5689d7fef
Roan Hofland <roan@roanh.dev>
2021-12-16 17:45:12 +0100

    Enable anti aliasing

--- ConvexMerger/src/dev/roanh/convexmerger/game/ConvexMerger.java
+++ ConvexMerger/src/dev/roanh/convexmerger/game/ConvexMerger.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.geom.Line2D;
@@ -75,6 +76,7 @@ public class ConvexMerger{
 		@Override
 		public void paintComponent(Graphics g1){
 			Graphics2D g = (Graphics2D)g1;
+			g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
 			renderGame(g);
 		}
