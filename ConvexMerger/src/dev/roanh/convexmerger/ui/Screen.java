ebd60ccff99525669b4b2993322b0e0158b108f3
Roan Hofland <roan@roanh.dev>
2022-01-06 01:29:32 +0100

    Add javadoc to the Screen class

--- ConvexMerger/src/dev/roanh/convexmerger/ui/Screen.java
+++ ConvexMerger/src/dev/roanh/convexmerger/ui/Screen.java
@@ -44,8 +44,17 @@ public abstract class Screen{
 	 * Space between the boxes.
 	 */
 	public static final int BOX_SPACING = 12;
+	/**
+	 * Inset into a drawn box and also the corner triangle size.
+	 */
 	public static final int BOX_INSETS = 10;
+	/**
+	 * Height of a box title header.
+	 */
 	public static final int BOX_HEADER_HEIGHT = 28;
+	/**
+	 * Text offset from the side of a box.
+	 */
 	public static final int BOX_TEXT_OFFSET = 4;
 	/**
 	 * Bottom right button polygon.
@@ -55,21 +64,45 @@ public abstract class Screen{
 	 * Bottom left button polygon.
 	 */
 	private Polygon leftPoly = null;
+	/**
+	 * Last mouse location.
+	 */
 	private Point2D lastLocation = new Point2D.Double();
+	/**
+	 * The active game context.
+	 */
 	private ConvexMerger context;
 	
+	/**
+	 * Constructs a new screen with the given game context.
+	 * @param context The game context.
+	 */
 	protected Screen(ConvexMerger context){
 		this.context = context;
 	}
 	
+	/**
+	 * Gets the game context this screen is associated with.
+	 * @return The game context.
+	 */
 	protected ConvexMerger getContext(){
 		return context;
 	}
 	
-	protected Screen switchScene(Screen next){
-		return context.switchScene(next);
+	/**
+	 * Switches the context from the current screen to the given screen.
+	 * @param next The screen to switch to.
+	 */
+	protected void switchScene(Screen next){
+		context.switchScene(next);
 	}
 
+	/**
+	 * Renders this screen with the given graphics and dimensions.
+	 * @param g The graphics context to use.
+	 * @param width The width of the screen.
+	 * @param height The height of the screen.
+	 */
 	public void render(Graphics2D g, int width, int height){
 		g.setColor(Theme.BACKGROUND);
 		g.fillRect(0, 0, width, height);
@@ -77,12 +110,33 @@ public abstract class Screen{
 		render(g, width, height, lastLocation);
 	}
 	
+	/**
+	 * Renders the content of this screen with the given graphics.
+	 * @param g The graphics context to use.
+	 * @param width The width of the screen.
+	 * @param height The height of the screen.
+	 * @param mouseLoc The current cursor location.
+	 */
 	protected abstract void render(Graphics2D g, int width, int height, Point2D mouseLoc);
 	
+	/**
+	 * Computes how wide the content should be given a screen
+	 * width, width usage ratio and maximum width. So effectively
+	 * this computes <code>min(ratio * width, max)</code>.
+	 * @param width The width of the screen.
+	 * @param ratio The fraction of the width to use.
+	 * @param max The maximum width.
+	 * @return The content width.
+	 */
 	protected static double getMaxWidth(int width, double ratio, int max){
 		return Math.min(ratio * width, max);
 	}
 	
+	/**
+	 * Draws the main screen title.
+	 * @param g The graphics context to use.
+	 * @param width The width of the screen.
+	 */
 	protected void drawTitle(Graphics2D g, int width){
 		g.setColor(Color.WHITE);
 		g.setFont(Theme.PRIDI_MEDIUM_30);
@@ -90,6 +144,16 @@ public abstract class Screen{
 		g.drawString(Constants.TITLE, (width - fm.stringWidth(Constants.TITLE)) / 2.0F, (GamePanel.TOP_SPACE + fm.getAscent() - fm.getDescent() - fm.getLeading()) / 2.0F);
 	}
 	
+	/**
+	 * Draws a titled box according to the given parameters.
+	 * @param g The graphics context to use.
+	 * @param gradient The gradient to use.
+	 * @param x The x location of the top left corner of the box.
+	 * @param y The y location of the top left corner of the box.
+	 * @param w The width of the box.
+	 * @param h The height of the box.
+	 * @param title The header title of the box.
+	 */
 	protected void drawTitledBox(Graphics2D g, Paint gradient, double x, double y, double w, double h, String title){
 		g.setColor(Theme.MENU_BODY);
 		drawBox(g, x, y, w, h);
@@ -106,10 +170,27 @@ public abstract class Screen{
 		
 	}
 	
+	/**
+	 * Draws a box according to the given parameters.
+	 * @param g The graphics context to use.
+	 * @param x The x location of the top left corner of the box.
+	 * @param y The y location of the top left corner of the box.
+	 * @param w The width of the box.
+	 * @param h The height of the box.
+	 */
 	protected void drawBox(Graphics2D g, double x, double y, double w, double h){
 		g.fill(computeBox(x, y, w, h, BOX_INSETS));
 	}
 	
+	/**
+	 * Computes the bounds of a box if draw according to the given parameters.
+	 * @param x The x location of the top left corner of the box.
+	 * @param y The y location of the top left corner of the box.
+	 * @param w The width of the box.
+	 * @param h The height of the box.
+	 * @param inset The box insets, see {@link #BOX_INSETS}.
+	 * @return The box bounds.
+	 */
 	protected Path2D computeBox(double x, double y, double w, double h, double inset){
 		Path2D path = new Path2D.Double(Path2D.WIND_NON_ZERO, 8);
 		path.moveTo(x, y + inset);
@@ -124,13 +205,33 @@ public abstract class Screen{
 		return path;
 	}
 	
+	/**
+	 * Renders a screen sub title.
+	 * @param g The graphics context to use.
+	 * @param width The width of the screen.
+	 * @param title The sub title to draw.
+	 */
 	protected void renderMenuTitle(Graphics2D g, int width, String title){
 		g.setFont(Theme.PRIDI_REGULAR_18);
 		FontMetrics fm = g.getFontMetrics();
 		g.drawString(title, Math.floorDiv(width, 2) - (TOP_MIDDLE_WIDTH / 2) + (TOP_MIDDLE_WIDTH - fm.stringWidth(title)) / 2.0F, TOP_SPACE + TOP_OFFSET - fm.getDescent() - TOP_MIDDLE_TEXT_OFFSET);
 	}
 	
+	/**
+	 * Fills the given area with the given text. The text is given
+	 * as a list of words and it is assumed that each word should
+	 * be separated by a single space. The words automatically get
+	 * wrapped and no words will go outside the bounding box.
+	 * @param g The graphics context to use.
+	 * @param rx The x coordinate of the top left corner of the area.
+	 * @param ry The y coordinate of the top left corner of the area.
+	 * @param width The width of the area.
+	 * @param height The height of the area.
+	 * @param text The list of words to draw.
+	 * @return The y coordinate of the last text baseline used.
+	 */
 	protected int fillText(Graphics2D g, int rx, int ry, int width, int height, List<String> text){
+		g.setClip(rx, ry, width, height);
 		FontMetrics fm = g.getFontMetrics();
 		int x = rx;
 		int y = ry + fm.getAscent();
@@ -147,10 +248,19 @@ public abstract class Screen{
 			g.drawString(word + " ", x, y);
 			x += w + fm.stringWidth(" ");
 		}
+		g.setClip(null);
 		return y;
 	}
 	
-	//state optional
+	/**
+	 * Renders the main interface, the top panel and button left and
+	 * right buttons.
+	 * @param g The graphics context to use.
+	 * @param width The width of the screen.
+	 * @param height The height of the screen.
+	 * @param state The game state to use to determine the gradient
+	 *        to use, can be <code>null</code>.
+	 */
 	protected void renderMainInterface(Graphics2D g, int width, int height, GameState state){
 		g.setColor(Theme.MENU_BODY);
 		int sideOffset = Math.floorDiv(width, 2) - (TOP_MIDDLE_WIDTH / 2);
@@ -255,6 +365,13 @@ public abstract class Screen{
 		g.draw(topPath);
 	}
 	
+	/**
+	 * Handles a mouse click on this screen. If this method
+	 * is overridden a super call needs to be made.
+	 * @param loc The location that was clicked.
+	 * @param width The width of the screen.
+	 * @param height The height of the screen.
+	 */
 	public void handleMouseClick(Point2D loc, int width, int height){
 		if(isLeftButtonEnabled() && leftPoly.contains(loc)){
 			handleLeftButtonClick();
@@ -263,25 +380,62 @@ public abstract class Screen{
 		}
 	}
 	
+	/**
+	 * Handles a mouse move on this screen. If this method
+	 * is overridden a super call needs to be made.
+	 * @param loc The current cursor location.
+	 * @param width The width of the screen.
+	 * @param height The height of the screen.
+	 */
 	public void handleMouseMove(Point2D loc, int width, int height){
 		lastLocation = loc;
 	}
 	
+	/**
+	 * Handles a key press event.
+	 * @param event The key press event.
+	 */
 	public void handleKeyPressed(KeyEvent event){
 	}
 
+	/**
+	 * Handles a key release event.
+	 * @param event The key release event.
+	 */
 	public void handleKeyReleased(KeyEvent event){
 	}
 	
+	/**
+	 * Checks if the bottom left button is enabled.
+	 * @return True if the bottom left button is enabled.
+	 */
 	protected abstract boolean isLeftButtonEnabled();
 	
+	/**
+	 * Checks if the bottom right button is enabled.
+	 * @return True if the bottom right button is enabled.
+	 */
 	protected abstract boolean isRightButtonEnabled();
 	
+	/**
+	 * Gets the text for the bottom left button.
+	 * @return The text for the bottom left button.
+	 */
 	protected abstract String getLeftButtonText();
 	
+	/**
+	 * Gets the text for the bottom right button.
+	 * @return The text for the bottom right button.
+	 */
 	protected abstract String getRightButtonText();
 	
+	/**
+	 * Handles the bottom left button being clicked.
+	 */
 	protected abstract void handleLeftButtonClick();
 	
+	/**
+	 * Handles the bottom right button being clicked.
+	 */
 	protected abstract void handleRightButtonClick();
 }
