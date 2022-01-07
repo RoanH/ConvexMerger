23150cc7ece57996d71a0ad3d11ada4f742582e4
Roan Hofland <roan@roanh.dev>
2022-01-07 16:36:38 +0100

    Document the Main class

--- ConvexMerger/src/dev/roanh/convexmerger/Main.java
+++ ConvexMerger/src/dev/roanh/convexmerger/Main.java
@@ -1,32 +1,24 @@
 package dev.roanh.convexmerger;
 
 import dev.roanh.convexmerger.ui.ConvexMerger;
-import dev.roanh.convexmerger.ui.NewGameMenu;
 import dev.roanh.util.Util;
 
+/**
+ * Main entry point for the application.
+ * @author Roan
+ */
 public class Main{
 
+	/**
+	 * Main subroutine that starts the game.
+	 * @param args No valid command line arguments.
+	 */
 	public static void main(String[] args){
 		Util.installUI();
 		
 		ConvexMerger game = new ConvexMerger();
 //		game.hostMultiplayerGame();
-		
-		//game.initialiseGame();
-		
-		
-		//game.switchScene(new NewGameMenu(game));
-		
-		
-		
-		
-		
+//		game.joinMultiplayerGame();
 		game.showGame();
-		
-		
-//		ConvexMerger game2 = new ConvexMerger();
-//		game2.joinMultiplayerGame();
-		
-		//game2.showGame();
 	}
 }
