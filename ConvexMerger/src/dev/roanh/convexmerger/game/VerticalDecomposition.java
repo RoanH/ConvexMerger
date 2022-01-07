fbdd09bbbc0ff7dab7ac624880eb53edb55a7032
Emu <e.greshkov@student.tue.nl>
2022-01-07 20:54:51 +0100

    Implement and document initial decomposition vertex class.

--- ConvexMerger/src/dev/roanh/convexmerger/game/VerticalDecomposition.java
+++ ConvexMerger/src/dev/roanh/convexmerger/game/VerticalDecomposition.java
@@ -70,7 +70,7 @@ public class VerticalDecomposition{
 	 * @param x The x-coordinate of the query point.
 	 * @param y The y-coordinate of the query point.
 	 * @return The convex object at the given position or
-	 *         <code>null</code> is there is no point at
+	 *         <code>null</code> is there is no convex object at
 	 *         the given position.
 	 */
 	public ConvexObject queryObject(double x, double y){
@@ -96,6 +96,204 @@ public class VerticalDecomposition{
 		}
 		
 		return lines;
+	}
+	
+	/**
+	 * Represents a vertex in the search structure of the decomposition.
+	 * @author Emu
+	 */
+	public class DecompVertex{
+		/**
+		 * The type of the vertex. 0 denotes leaf, 1 denotes point, 2 denotes line segment.
+		 */
+		private int type;
+		
+		/**
+		 * The children of the vertex in the search structure.
+		 */
+		private DecompVertex left, right;
+		
+		/**
+		 * If the vertex is a leaf, it points to a trapezoid.
+		 */
+		private Trapezoid trapezoid;
+		
+		/**
+		 * If the vertex represents a point, it points to it.
+		 */
+		private Point2D point;
+		
+		/**
+		 * If the vertex represents a segment, it points to it.
+		 */
+		private Line2D segment;
+		
+		/**
+		 * Constructs a vertex with no children and only a type.
+		 * @param type The type of the vertex. 0 denotes leaf, 1 denotes point, 2 denotes line segment.
+		 */
+		public DecompVertex(int type){
+			this.type  = type;
+			this.left  = null;
+			this.right = null;
+		}
+		
+		/**
+		 * 
+		 * @param type The type of the vertex. 0 denotes leaf, 1 denotes point, 2 denotes line segment.
+		 * @param left The left child of the vertex.
+		 * @param right The right child of the vertex.
+		 */
+		
+		/**
+		 * Constructs a Decomposition Vertex of expected type 0 (leaf) with a linked trapezoid.
+		 * @param type The type of the vertex. 0 denotes leaf, 1 denotes point, 2 denotes line segment.
+		 * @param trapezoid The trapezoid to link to.
+		 */
+		public DecompVertex(int type, Trapezoid trapezoid){
+			this(type, null, null, trapezoid, null, null);
+		}
+		
+		/**
+		 * Constructs a Decomposition Vertex of expected type 1 (point) with a corresponding point.
+		 * @param type The type of the vertex. 0 denotes leaf, 1 denotes point, 2 denotes line segment.
+		 * @param left The left child of the vertex.
+		 * @param right The right child of the vertex.
+		 * @param point The corresponding point in the decomposition.
+		 */
+		public DecompVertex(int type, DecompVertex left, DecompVertex right, Point2D point){
+			this(type, left, right, null, point, null);
+		}
+		
+		/**
+		 * Constructs a Decomposition Vertex of expected type 2 (segment) with a corresponding line segment.
+		 * @param type The type of the vertex. 0 denotes leaf, 1 denotes point, 2 denotes line segment.
+		 * @param left The left child of the vertex.
+		 * @param right The right child of the vertex.
+		 * @param segment The corresponding line segment in the decomposition.
+		 */
+		public DecompVertex(int type, DecompVertex left, DecompVertex right, Line2D segment){
+			this(type, left, right, null, null, segment);
+		}
+		
+		/**
+		 * Unified constructor for Decomposition vertices.
+		 * @param type The type of the vertex. 0 denotes leaf, 1 denotes point, 2 denotes line segment.
+		 * @param left The left child of the vertex.
+		 * @param right The right child of the vertex.
+		 * @param trapezoid The trapezoid for leaf nodes to link to.
+		 * @param point The point for point nodes to link to.
+		 * @param segment The line segment for segment nodes to link to.
+		 */
+		public DecompVertex(int type, DecompVertex left, DecompVertex right, Trapezoid trapezoid, Point2D point, Line2D segment){
+			this.type  = type;
+			if(type == 0){
+				this.left  = null;
+				this.right = null;
+				this.trapezoid = trapezoid;
+				this.point = null;
+				this.segment = null;
+			}else if(type < 3){
+				this.left = left;
+				this.right = right;
+				if(type == 1){
+					this.point = point;
+					this.trapezoid = null;
+					this.segment = null;
+				}
+				if(type == 2){
+					this.trapezoid = null;
+					this.point = null;
+					this.segment = segment;
+				}
+			}
+		}
+		
+		/**
+		 * Queries which trapezoid a point lies in.
+		 * Leaf nodes return their linked trapezoid.
+		 * Point nodes check whether the point lies to the left or right 
+		 * and return the result for the corresponding child.
+		 * Segment nodes check whether the poitn lies above or below the segment
+		 * and returns the result of the left child if above, or the right child if below.  
+		 * @param query The point whose containing trapezoid is to be determined.
+		 * @return The trapezoid that contains the query point.
+		 */
+		public Trapezoid queryPoint(Point2D query){
+			if(type == 0){
+				return trapezoid;
+			}else if(type == 1){
+				return null;//TODO
+			}else if(type == 2){
+				return null;//TODO
+			}else{
+				return null;
+			}
+		}
+		
+		/**
+		 * Gets the left child of the vertex
+		 * @return The left child of the vertex, or null if there is none.
+		 */
+		public DecompVertex getLeftChild(){
+			return left;
+		}
+		
+		/**
+		 * Sets the right child of the vertex
+		 * @param vert The vertex to set the left child to.
+		 */
+		public void setLeftChild(DecompVertex vert){
+			this.left = vert;
+		}
+		
+		/**
+		 * Sets the left child of the vertex to null.
+		 */
+		public void removeLeftChild(){
+			this.left = null;
+		}
+		
+		/**
+		 * Gets the right child of the vertex.
+		 * @return
+		 */
+		public DecompVertex getRightChild(){
+			return right;
+		}
+		
+		/**
+		 * Sets the right child of the vertex.
+		 * @param vert The vertex to set the right child to.
+		 */
+		public void setRightChild(DecompVertex vert){
+			this.right = vert;
+		}
+		
+		/**
+		 * Sets the right child of the vertex to null.
+		 */
+		public void removeRightChild(){
+			this.right = null;
+		}
+		
+		/**
+		 * Gets the trapezoid that the vertex points to.
+		 * @return The trapezoid that the vertex points to. Null if not leaf.
+		 */
+		public Trapezoid getTrapezoid(){
+			return trapezoid;
+		}
+		
+		/**
+		 * Sets the trapezoid that the vertex points to to a given trapezoid, if the vertex is a leaf.
+		 * @param trapezoid The trapezoid to associate with the vertex.
+		 */
+		public void setTrapezoid(Trapezoid trapezoid){
+			if(type == 0){
+				this.trapezoid = trapezoid;
+			}
+		}
 	}
 	
 	/**
@@ -126,7 +324,7 @@ public class VerticalDecomposition{
 		 * @param top2 The other point of the bottom bounding line of the trapezoid.
 		 * @param neighbours The neighbouring trapezoids of the constructed trapezoid. 
 		 */
-		public Trapezoid (Point2D left, Point2D right, Point2D bot1, Point2D bot2, Point2D top1, Point2D top2, List<Trapezoid> neighbours){
+		public Trapezoid(Point2D left, Point2D right, Point2D bot1, Point2D bot2, Point2D top1, Point2D top2, List<Trapezoid> neighbours){
 			topLeft = Double.compare(top1.getX(), top2.getX()) == 0 ? (top1.getY() < top2.getY() ? top1 : top2) : (top1.getX() < top2.getX() ? top1 : top2);  
 			topRight = topLeft.equals(top1) ? top2 : top1;
 			botLeft = Double.compare(bot1.getX(), bot2.getX()) == 0 ? (bot1.getY() < bot2.getY() ? bot1 : bot2) : (bot1.getX() < bot2.getY() ? bot1 : bot2);
@@ -137,8 +335,8 @@ public class VerticalDecomposition{
 		}
 		
 		/**
-		 * Getter for the neighbours of the trapezoid
-		 * @return the neighbours of the trapezoid
+		 * Getter for the neighbours of the trapezoid.
+		 * @return the neighbours of the trapezoid.
 		 */
 		public List<Trapezoid> getNeighbours(){
 			return neighbours;
@@ -154,7 +352,7 @@ public class VerticalDecomposition{
 		
 		/**
 		 * Removes a neighbour from the list of neighbours.
-		 * @param neighbour the neighbour to be removed
+		 * @param neighbour the neighbour to be removed.
 		 */
 		public void removeNeighbour (Trapezoid neighbour){
 			neighbours.remove(neighbours.indexOf(neighbour));
@@ -162,7 +360,7 @@ public class VerticalDecomposition{
 		
 		/**
 		 * Removes this trapezoid from the neighbour lists of all neighbours.
-		 * Useful for when we want to delete a 
+		 * Useful for when we want to delete a Trapezoid.
 		 */
 		public void freeNeighbours (){
 			for(Trapezoid t : neighbours){
@@ -171,14 +369,21 @@ public class VerticalDecomposition{
 		}
 		
 		/**
-		 * Computes and outputs the decomposition lines related to this trapezoid
+		 * Computes and outputs the decomposition lines related to this trapezoid.
 		 * @return The vertical decomposition lines, unless one or more of the bounding lines is vertical.
 		 */
 		public List<Line2D> getDecompLines(){
 			List<Line2D> verticalLines = new ArrayList<Line2D>(); 
 		
 			//TODO: figure out a better way to handle vertical top or bottom lines.
-			if(Double.compare(topLeft.getX(), topRight.getX()) == 0 || Double.compare(botLeft.getX(), botRight.getX()) == 0){
+			if(Double.compare(topLeft.getX(), topRight.getX()) == 0){
+//				double xRatioBot = (topLeft.getX() - botLeft.getX()) / (botRight.getX() - botLeft.getX());
+//				Point2D topPoint = Point2D.Double(leftPoint.get(X));
+//				verticalLines.add(null);
+//				
+				return verticalLines;
+			}
+			if( Double.compare(botLeft.getX(), botRight.getX()) == 0){
 				return verticalLines;
 			}
 			
