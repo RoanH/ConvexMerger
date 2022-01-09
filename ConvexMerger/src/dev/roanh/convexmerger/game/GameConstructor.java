0e34de5fc3e1a463a190c3c73049847a715259ba
Roan Hofland <roan@roanh.dev>
2022-01-09 04:16:57 +0100

    Document the GameConstructor interface

--- ConvexMerger/src/dev/roanh/convexmerger/game/GameConstructor.java
+++ ConvexMerger/src/dev/roanh/convexmerger/game/GameConstructor.java
@@ -1,6 +1,16 @@
 package dev.roanh.convexmerger.game;
 
+/**
+ * A simple interface used to execute the longer
+ * calculations required to start a game on the
+ * main game thread.
+ * @author Roan
+ */
 public abstract interface GameConstructor{
 
+	/**
+	 * Constructs the game state for the configured game.
+	 * @return The game state.
+	 */
 	public abstract GameState create();
 }
