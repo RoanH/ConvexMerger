62f627c9f20eff2a2e728bbc10f891ff67d33136
Roan Hofland <roan@roanh.dev>
2022-01-12 00:02:14 +0100

    Make the main menu less empty

--- ConvexMerger/src/dev/roanh/convexmerger/ui/MainMenu.java
+++ ConvexMerger/src/dev/roanh/convexmerger/ui/MainMenu.java
@@ -1,8 +1,14 @@
 package dev.roanh.convexmerger.ui;
 
 import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
+import java.util.concurrent.ThreadLocalRandom;
+
+import dev.roanh.convexmerger.game.ConvexObject;
+import dev.roanh.convexmerger.ui.Theme.PlayerTheme;
 
 /**
  * Game main menu screen.
@@ -37,6 +43,10 @@ public class MainMenu extends Screen{
 	 * Bounds of the quit button.
 	 */
 	private Path2D quit = new Path2D.Double();
+	/**
+	 * Main menu show case objects.
+	 */
+	private Path2D[] objects;
 
 	/**
 	 * Constructs a new main menu with the given game context.
@@ -44,6 +54,33 @@ public class MainMenu extends Screen{
 	 */
 	protected MainMenu(ConvexMerger context){
 		super(context);
+		ThreadLocalRandom r = ThreadLocalRandom.current();
+		objects = new Path2D[]{
+			new ConvexObject(
+				r.nextDouble(), r.nextDouble(),
+				1.0D + r.nextDouble(), r.nextDouble(),
+				r.nextDouble(), 1.0D + r.nextDouble(),
+				1.0D + r.nextDouble(), 1.0D + r.nextDouble()
+			).getShape(),
+			new ConvexObject(
+				r.nextDouble(), r.nextDouble(),
+				1.0D + r.nextDouble(), r.nextDouble(),
+				r.nextDouble(), 1.0D + r.nextDouble(),
+				1.0D + r.nextDouble(), 1.0D + r.nextDouble()
+			).getShape(),
+			new ConvexObject(
+				r.nextDouble(), r.nextDouble(),
+				1.0D + r.nextDouble(), r.nextDouble(),
+				r.nextDouble(), 1.0D + r.nextDouble(),
+				1.0D + r.nextDouble(), 1.0D + r.nextDouble()
+			).getShape(),
+			new ConvexObject(
+				r.nextDouble(), r.nextDouble(),
+				1.0D + r.nextDouble(), r.nextDouble(),
+				r.nextDouble(), 1.0D + r.nextDouble(),
+				1.0D + r.nextDouble(), 1.0D + r.nextDouble()
+			).getShape()
+		};
 	}
 
 	@Override
@@ -57,6 +94,37 @@ public class MainMenu extends Screen{
 		join = drawButton(g, "Join Multiplayer", (width - BUTTON_WIDTH) / 2.0D, TOP_MIDDLE_OFFSET + BOX_SPACING * 2.0D + TOP_SPACE + (BOX_SPACING + BUTTON_HEIGHT) * 2.0D, BUTTON_WIDTH, BUTTON_HEIGHT, mouseLoc);
 		info = drawButton(g, "Info & Rules", (width - BUTTON_WIDTH) / 2.0D, TOP_MIDDLE_OFFSET + BOX_SPACING * 2.0D + TOP_SPACE + (BOX_SPACING + BUTTON_HEIGHT) * 3.0D, BUTTON_WIDTH, BUTTON_HEIGHT, mouseLoc);
 		quit = drawButton(g, "Quit", (width - BUTTON_WIDTH) / 2.0D, TOP_MIDDLE_OFFSET + BOX_SPACING * 2.0D + TOP_SPACE + (BOX_SPACING + BUTTON_HEIGHT) * 4.0D, BUTTON_WIDTH, BUTTON_HEIGHT, mouseLoc);
+		
+		double w = (width - BUTTON_WIDTH - TOP_SIDE_TRIANGLE * 4.0D) / 2.0D;
+		double h = (height - TOP_SPACE - TOP_SIDE_TRIANGLE * 2.0D - BOX_INSETS) / 2.0D;
+		AffineTransform transform = AffineTransform.getScaleInstance(0.5D * w, 0.5D * h);
+		
+		g.translate(TOP_SIDE_TRIANGLE, TOP_SPACE + TOP_SIDE_TRIANGLE);
+		render(g, objects[0].createTransformedShape(transform), PlayerTheme.P1);
+		
+		g.translate(0.0D, h);
+		render(g, objects[1].createTransformedShape(transform), PlayerTheme.P2);
+		
+		g.translate(TOP_SIDE_TRIANGLE * 2.0D + BUTTON_WIDTH + w, 0.0D);
+		render(g, objects[2].createTransformedShape(transform), PlayerTheme.P3);
+		
+		g.translate(0.0D, -h);
+		render(g, objects[3].createTransformedShape(transform), PlayerTheme.P4);
+	}
+	
+	/**
+	 * Renders the given shape in
+	 * the convex object style.
+	 * @param g The graphics context to use.
+	 * @param obj The shape to render.
+	 * @param theme The theme to use.
+	 */
+	private void render(Graphics2D g, Shape obj, PlayerTheme theme){
+		g.setColor(theme.getBody());
+		g.fill(obj);
+		g.setStroke(Theme.POLY_STROKE);
+		g.setColor(theme.getOutline());
+		g.draw(obj);
 	}
 	
 	@Override
