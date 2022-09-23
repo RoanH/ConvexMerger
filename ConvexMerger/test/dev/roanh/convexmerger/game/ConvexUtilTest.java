55cdbf6b8d0b42839e2666d0e18e1c80a70c01e8
Roan Hofland <roan@roanh.dev>
2022-09-23 22:58:52 +0200

    Move most tests to real unit tests

--- ConvexMerger/test/dev/roanh/convexmerger/game/ConvexUtilTest.java
+++ ConvexMerger/test/dev/roanh/convexmerger/game/ConvexUtilTest.java
@@ -10,10 +10,119 @@ import org.junit.jupiter.api.Test;
 
 public class ConvexUtilTest{
 
+	@Test
+	public void normalTest(){
+		mergeTest(
+			new ConvexObject(ConvexUtil.computeConvexHull(Arrays.asList(
+				new Point2D.Double(33, 118),
+				new Point2D.Double(57, 178),
+				new Point2D.Double(98, 236),
+				new Point2D.Double(180, 270),
+				new Point2D.Double(204, 171),
+				new Point2D.Double(175, 106),
+				new Point2D.Double(146, 77),
+				new Point2D.Double(116, 65),
+				new Point2D.Double(38, 70)
+			))),
+			new ConvexObject(ConvexUtil.computeConvexHull(Arrays.asList(
+				new Point2D.Double(100 + 116, 177),
+				new Point2D.Double(100 + 133, 212),
+				new Point2D.Double(100 + 263, 217),
+				new Point2D.Double(100 + 286, 150),
+				new Point2D.Double(100 + 281, 65),
+				new Point2D.Double(100 + 256, 24),
+				new Point2D.Double(100 + 219, 42),
+				new Point2D.Double(100 + 181, 71),
+				new Point2D.Double(100 + 133, 113)
+			)))
+		);
+	}
 	
+	@Test
+	public void endPointTest(){
+		mergeTest(
+			new ConvexObject(ConvexUtil.computeConvexHull(Arrays.asList(
+				new Point2D.Double(30, 250),
+				new Point2D.Double(100, 100),
+				new Point2D.Double(100, 200)
+			))),
+			new ConvexObject(ConvexUtil.computeConvexHull(Arrays.asList(
+				new Point2D.Double(100 + 30, 100),
+				new Point2D.Double(100 + 100, 110),
+				new Point2D.Double(100 + 100, 200)
+			)))
+		);
+	}
 	
+	@Test
+	public void parallelTest(){
+		mergeTest(
+			new ConvexObject(ConvexUtil.computeConvexHull(Arrays.asList(
+				new Point2D.Double(30, 100),
+				new Point2D.Double(100, 80),
+				new Point2D.Double(100, 200)
+			))),
+			new ConvexObject(ConvexUtil.computeConvexHull(Arrays.asList(
+				new Point2D.Double(100 + 30, 100),
+				new Point2D.Double(100 + 100, 110),
+				new Point2D.Double(100 + 100, 200),
+				new Point2D.Double(100 + 50, 150)
+			)))
+		);
+	}
 	
+	@Test
+	public void horizontalBoxColinTest(){
+		mergeTest(
+			new ConvexObject(ConvexUtil.computeConvexHull(Arrays.asList(
+				new Point2D.Double(30, 200),
+				new Point2D.Double(30, 100),
+				new Point2D.Double(100, 100),
+				new Point2D.Double(100, 200)
+			))),
+			new ConvexObject(ConvexUtil.computeConvexHull(Arrays.asList(
+				new Point2D.Double(100 + 30, 200),
+				new Point2D.Double(100 + 30, 100),
+				new Point2D.Double(100 + 100, 100),
+				new Point2D.Double(100 + 100, 200)
+			)))
+		);
+	}
 	
+	@Test
+	public void verticalBoxColinTest(){
+		mergeTest(
+			new ConvexObject(ConvexUtil.computeConvexHull(Arrays.asList(
+				new Point2D.Double(30, 200),
+				new Point2D.Double(30, 100),
+				new Point2D.Double(100, 100),
+				new Point2D.Double(100, 200)
+			))),
+			new ConvexObject(ConvexUtil.computeConvexHull(Arrays.asList(
+				new Point2D.Double(30, 130 + 200),
+				new Point2D.Double(30, 130 + 100),
+				new Point2D.Double(100, 130 + 100),
+				new Point2D.Double(100, 130 + 200)
+			)))
+		);
+	}
+	
+	@Test
+	public void mergeLineOverlapTest(){
+		mergeTest(
+			new ConvexObject(ConvexUtil.computeConvexHull(Arrays.asList(
+				new Point2D.Double(50, -200),
+				new Point2D.Double(30, 100),
+				new Point2D.Double(100, 100),
+				new Point2D.Double(100, 300)
+			))),
+			new ConvexObject(ConvexUtil.computeConvexHull(Arrays.asList(
+				new Point2D.Double(100 + 50, 140),
+				new Point2D.Double(100 + 30, 100),
+				new Point2D.Double(100 + 300, 150)
+			)))
+		);
+	}
 	
 	@Test
 	public void overflowTest(){
