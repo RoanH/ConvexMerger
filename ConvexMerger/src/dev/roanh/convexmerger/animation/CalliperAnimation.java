295c7c5af23ffa60c2d64646d274de04826bd1ee
Roan Hofland <roan@roanh.dev>
2022-09-25 17:59:36 +0200

    Document the calliper animation

--- ConvexMerger/src/dev/roanh/convexmerger/animation/CalliperAnimation.java
+++ ConvexMerger/src/dev/roanh/convexmerger/animation/CalliperAnimation.java
@@ -17,11 +17,28 @@ import dev.roanh.convexmerger.game.ConvexUtil;
  * @author Roan
  */
 public class CalliperAnimation implements Animation{
+	/**
+	 * The number of milliseconds the animation runs for.
+	 */
 	private static final float DURATION = 10000.0F;
+	/**
+	 * The epoch millis start time of the animation.
+	 */
 	private long start;
+	/**
+	 * The convex object point set.
+	 */
 	private List<Point2D> points;
+	/**
+	 * Cached current index in the points list.
+	 */
 	private int index = 0;
 	
+	/**
+	 * Constructs a new calliper animation for the given object.
+	 * Note: this animation only renders the calliper and not the object.
+	 * @param obj The object to show a calliper for.
+	 */
 	public CalliperAnimation(ConvexObject obj){
 		points = obj.getPoints();
 		start = System.currentTimeMillis();
@@ -55,6 +72,14 @@ public class CalliperAnimation implements Animation{
 		return elapsed < DURATION;
 	}
 	
+	/**
+	 * Draws an 'infinite' line segment through the two given points.
+	 * @param g The graphics context to use.
+	 * @param x1 The x-coordinate of the first point.
+	 * @param y1 The y-coordinate of the first point.
+	 * @param x2 The x-coordinate of the second point.
+	 * @param y2 The y-coordinate of the second point.
+	 */
 	private void drawLine(Graphics2D g, double x1, double y1, double x2, double y2){
 		double coef = x1 == x2 ? 0.0D : (y2 - y1) / (x2 - x1);
 		double base = y1 - x1 * coef;
