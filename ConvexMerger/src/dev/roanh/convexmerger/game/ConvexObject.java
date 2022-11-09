3c3584cfd30f164888f2d874b8252ab662383845
Roan Hofland <roan@roanh.dev>
2022-11-09 02:51:44 +0100

    Properly apply the GPL
    Doesn't functionally change anything, but this is properly in line with
    the instructions.
    
    Signed-off-by: Roan Hofland <roan@roanh.dev>

--- ConvexMerger/src/dev/roanh/convexmerger/game/ConvexObject.java
+++ ConvexMerger/src/dev/roanh/convexmerger/game/ConvexObject.java
@@ -1,3 +1,21 @@
+/*
+ * ConvexMerger:  An area maximisation game based on the idea of merging convex shapes.
+ * Copyright (C) 2021  Roan Hofland (roan@roanh.dev), Emiliyan Greshkov and contributors.
+ * GitHub Repository: https://github.com/RoanH/ConvexMerger
+ *
+ * ConvexMerger is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ConvexMerger is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package dev.roanh.convexmerger.game;
 
 import java.awt.Graphics2D;
