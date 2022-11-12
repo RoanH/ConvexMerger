0d94db5158543de7a10ee7ac4cec523c1be73421
Roan Hofland <roan@roanh.dev>
2022-11-12 16:52:42 +0100

    Complete the renderable object documentation

--- ConvexMerger/src/dev/roanh/convexmerger/game/RenderableObject.java
+++ ConvexMerger/src/dev/roanh/convexmerger/game/RenderableObject.java
@@ -1,9 +1,32 @@
+/*
+ * ConvexMerger:  An area maximisation game based on the idea of merging convex shapes.
+ * Copyright (C) 2021  Roan Hofland (roan@roanh.dev), Emiliyan Greshkov and contributors.
+ * GitHub Repository: https://github.com/RoanH/ConvexMerger
+ *
+ * ConvexMerger is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ConvexMerger is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package dev.roanh.convexmerger.game;
 
 import java.awt.Graphics2D;
 
 import dev.roanh.convexmerger.animation.Animation;
 
+/**
+ * Represents an object that can be rendered and animated.
+ * @author Roan
+ * @see Animation
+ */
 public abstract class RenderableObject{
 	/**
 	 * The active animation for this object.
@@ -42,6 +65,11 @@ public abstract class RenderableObject{
 		}
 	}
 	
+	/**
+	 * Renders this object or runs its animation
+	 * if it has one set.
+	 * @param g The graphics context to use.
+	 */
 	public void renderOrAnimate(Graphics2D g){
 		if(hasAnimation()){
 			runAnimation(g);
