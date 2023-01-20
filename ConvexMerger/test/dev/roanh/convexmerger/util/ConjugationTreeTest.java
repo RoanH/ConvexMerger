fea6feef15f1417fae013a59e8c90d1f8f0854dc
Roan Hofland <roan@roanh.dev>
2023-01-20 18:02:03 +0100

    Improve conjugation tree unit tests

--- ConvexMerger/test/dev/roanh/convexmerger/util/ConjugationTreeTest.java
+++ ConvexMerger/test/dev/roanh/convexmerger/util/ConjugationTreeTest.java
@@ -25,7 +25,6 @@ import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
@@ -33,9 +32,7 @@ import org.junit.jupiter.api.Timeout;
 
 import dev.roanh.convexmerger.Constants;
 import dev.roanh.convexmerger.game.ConvexObject;
-import dev.roanh.convexmerger.game.GameState;
 import dev.roanh.convexmerger.game.PlayfieldGenerator;
-import dev.roanh.convexmerger.player.GreedyPlayer;
 
 public class ConjugationTreeTest{
 	private static final List<Point2D> testPoints = Arrays.asList(
@@ -58,44 +55,13 @@ public class ConjugationTreeTest{
 	@Test
 	public void constructionPoints(){
 		ConjugationTree<Void> tree = new ConjugationTree<Void>(testPoints);
-		
 		List<ConjugationTree<Void>> leaves = tree.streamLeafCells().collect(Collectors.toList());
-		assertEquals(16, leaves.size());
-		
-		for(ConjugationTree<Void> leaf : leaves){
-			//all leaves have no bisector and thus no points
-			assertEquals(0, leaf.getPoints().size());
-			assertNull(leaf.getBisector());
-			
-			//all leaves are at depth 4
-			assertEquals(4, leaf.getDepth());
-		}
-		
-		//all internal nodes have exactly one point
-		Deque<ConjugationTree<Void>> nodes = new LinkedList<ConjugationTree<Void>>();
-		while(!nodes.isEmpty()){
-			ConjugationTree<Void> node = nodes.pop();
-			if(!node.isLeafCell()){
-				assertEquals(1, node.getPoints().size());
-			}
-		}
-		
-		//the root is at depth 0
-		assertEquals(0, tree.getDepth());
-	}
-	
-	@Test
-	public void constructionConjugates(){
-		ConjugationTree<Void> tree = new ConjugationTree<Void>(testPoints);
 
 		assertEquals(31L, tree.streamCells().count());
+		assertEquals(16, leaves.size());
+		leaves.forEach(leaf->assertEquals(4, leaf.getDepth()));
 		
-		//assert that all bisectors are also conjugates
-		tree.streamCells().forEach(cell->{
-			if(cell.getDepth() > 0 && !cell.isLeafCell()){
-				assertNotNull(ConvexUtil.interceptClosed(cell.getBisector(), cell.getParent().getBisector()));
-			}
-		});
+		testTree(tree);
 	}
 	
 	@Test
@@ -178,9 +144,37 @@ public class ConjugationTreeTest{
 	}
 	
 	private void testConstructionSeed(String seed){
-		List<ConvexObject> objects = new PlayfieldGenerator(seed).generatePlayfield();
-		SegmentPartitionTree.TYPE_CONJUGATION_TREE.fromObjects(objects);
+		testTree(new ConjugationTree<Void>(new PlayfieldGenerator(seed).generatePlayfield().stream().flatMap(obj->{
+			return obj.getPoints().stream();
+		}).collect(Collectors.toList())));
+	}
+	
+	private void testTree(ConjugationTree<?> tree){
+		List<ConjugationTree<?>> leaves = tree.streamLeafCells().collect(Collectors.toList());
+		
+		for(ConjugationTree<?> leaf : leaves){
+			//all leaves have no bisector and thus no points
+			assertEquals(0, leaf.getPoints().size());
+			assertNull(leaf.getBisector());
+		}
+		
+		//all internal nodes have exactly one point
+		Deque<ConjugationTree<?>> nodes = new LinkedList<ConjugationTree<?>>();
+		while(!nodes.isEmpty()){
+			ConjugationTree<?> node = nodes.pop();
+			if(!node.isLeafCell()){
+				assertEquals(1, node.getPoints().size());
+			}
+		}
+		
+		//assert that all bisectors are also conjugates
+		tree.streamCells().forEach(cell->{
+			if(cell.getDepth() > 0 && !cell.isLeafCell()){
+				assertNotNull(ConvexUtil.interceptClosed(cell.getBisector(), cell.getParent().getBisector()));
+			}
+		});
+		
+		//the root is at depth 0
+		assertEquals(0, tree.getDepth());
 	}
 }
-
-
